dfb34d893c8a3260ad78ebbb1d373c71619d3115
math4tots <math4tots@users.noreply.github.com>
2024-02-28 21:56:23 +0700

    Delete the evaluator
    Will prefer translator - TODO

--- src/extension/run.ts
+++ src/extension/run.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import * as yal from '../lang/yal';
 import { getSelectionOrAllText, writeToNewEditor } from './utils';
-import { BASE_SCOPE, evaluate } from '../lang/evaluator';
+// import { BASE_SCOPE, evaluate } from '../lang/evaluator';
 
 export async function runCommand() {
   const editor = vscode.window.activeTextEditor;
@@ -10,20 +10,20 @@ export async function runCommand() {
   }
   const text = getSelectionOrAllText(editor);
   const file = yal.parse(editor.document.uri, text);
-  const scope = yal.newScope(BASE_SCOPE);
-  await writeToNewEditor(emit => {
-    scope['print'] = {
-      isConst: true, value: (_: yal.Value, args: yal.Value[]) =>
-        (args.length > 0 ? emit(`${yal.str(args[0])}\n`) : 0, null)
-    };
-    try {
-      evaluate(file, scope);
-    } catch (e) {
-      if (e instanceof Error && e.stack) {
-        emit(e.stack);
-      } else {
-        emit(`ERROR: ${e}`);
-      }
-    }
-  });
+  // const scope = yal.newScope(BASE_SCOPE);
+  // await writeToNewEditor(emit => {
+  //   scope['print'] = {
+  //     isConst: true, value: (_: yal.Value, args: yal.Value[]) =>
+  //       (args.length > 0 ? emit(`${yal.str(args[0])}\n`) : 0, null)
+  //   };
+  //   try {
+  //     evaluate(file, scope);
+  //   } catch (e) {
+  //     if (e instanceof Error && e.stack) {
+  //       emit(e.stack);
+  //     } else {
+  //       emit(`ERROR: ${e}`);
+  //     }
+  //   }
+  // });
 }
--- src/lang/yal.ts
+++ src/lang/yal.ts
@@ -2,12 +2,6 @@ export { parse } from './parser';
 export { lex, Token, TokenType, Position, Range } from './lexer';
 export { Location, Node } from './ast';
 export * as ast from './ast';
-export {
-  Value, YALClass, YALInstance, YALFunction,
-  getClass, callMethod, isTruthy,
-  str, repr,
-} from './value';
-export { Variable, Scope, newScope } from './evaluator';
 export { RuntimeError, errorStack } from './error';
 export { AnnotationError, Annotator, Reference } from './annotator';
 export {
