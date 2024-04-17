d0c707945d44d72a066817019960412cc2deafea
Kyumin Kim <math4tots@users.noreply.github.com>
2024-04-17 21:07:47 +0700

    kill the old extension stuff

--- src/lang/yal.ts
+++ src/lang/yal.ts
@@ -2,17 +2,4 @@ export { parse } from './parser';
 export { lex, Token, TokenType, Position, Range } from './lexer';
 export { Location, Node } from './ast';
 export * as ast from './ast';
-export {
-  AnnotationError, Annotator, Reference, annotateDocument,
-  CallInstance,
-} from './annotator';
-export {
-  Value, Method, Instance,
-  Type,
-  AnyType,
-  NilType, BoolType, NumberType, StringType,
-  ListType, FunctionType,
-  ClassType, InterfaceType, ModuleType,
-  reprValue, strValue,
-} from './type';
 export { LIBRARY_URIS } from './paths';
