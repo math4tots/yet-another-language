4e0341b8f6a4c4220952a0fa253708c1f0381198
Kyumin Kim <math4tots@users.noreply.github.com>
2024-06-12 12:19:21 +0800

    Update parse.ts

--- src/extension/parse.ts
+++ src/extension/parse.ts
@@ -3,24 +3,20 @@ import { getSelectionOrAllText, writeToNewEditor } from './utils';
 import { parse } from '../lang3/frontend/parser';
 import { Node, ParseError } from '../lang3/frontend/ast';
 
-function stripLocationsAndAddType(obj: any) {
-  if (obj && typeof obj === 'object') {
-    if (obj instanceof ParseError) {
-      // keep
-    } else if (Array.isArray(obj)) {
-      for (const item of obj) stripLocationsAndAddType(item);
-    } else {
-      if (obj instanceof Node) {
-        (obj as any).TYPE = obj.constructor.name;
-      }
-      for (const key in obj) {
-        if (key === 'location') {
-          delete (obj as any)[key];
-        } else {
-          stripLocationsAndAddType((obj as any)[key]);
-        }
+function format(obj: any): any {
+  if (obj instanceof Node) {
+    const ret: any = Object.create(null);
+    ret.TYPE = obj.constructor.name;
+    for (const key in obj) {
+      if (key !== 'location') {
+        ret[key] = format((obj as any)[key]);
       }
     }
+    return ret;
+  } else if (Array.isArray(obj)) {
+    return obj.map(x => format(x));
+  } else {
+    return obj;
   }
 }
 
@@ -31,8 +27,7 @@ export async function parseCommand() {
   }
   const text = getSelectionOrAllText(editor);
   const file = parse(text, '<input>');
-  stripLocationsAndAddType(file);
-  const string = JSON.stringify(file);
+  const string = JSON.stringify(format(file));
 
   await writeToNewEditor(emit => { emit(string); }, 'json');
 }
